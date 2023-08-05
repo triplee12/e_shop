6ef3cc43eed2ee1ca051f0b37e5c066a0ebf3497
triplee12 <tripleeoliver@gmail.com>
2023-08-05 10:57:39 +0100

    Implement customer order views

--- eshop/orders/views.py
+++ eshop/orders/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+"""Order views."""
 
-# Create your views here.
+from django.shortcuts import render
+from cart.cart import Cart
+from .models import OrderItem
+from .forms import OrderCreateForm
+
+
+def order_create(request):
+    """Create a new order."""
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(
+                    order=order,
+                    product=item['product'],
+                    price=item['price'],
+                    quantity=item['quantity']
+                )
+            # clear the cart
+            cart.clear()
+            return render(
+                request,
+                'orders/order/created.html',
+                {"order": order}
+            )
+    else:
+        form = OrderCreateForm()
+        return render(
+            request,
+            'orders/order/create.html',
+            {"cart": cart, "form": form}
+        )
